655259d1d2cc6fa8fe75a8815f57cd89c743b6ba
faezeh7727 <gfaeze561@gmail.com>
2025-10-28 13:49:43 -0700

    first commit1

---
+++ services.js
@@ -0,0 +1,80 @@
+<div class=" container">
+  <div class="tab-content" id="nav-classic-tab">
+  <article class="feature-images-box">
+  <img class="feature-images" src="/images/cc-classic.webp" alt="featureimage">
+</article>
+<article class="feature-content">
+ <h3 class="services">خدمات تپسی برای کسب و کارها</h3>
+ <p class="feature-text">پنل سازمانی تپسی، سامانه‌ای تحت وب است که امکان درخواست هم‌زمان و سهولت استفاده را فراهم می‌کند.
+با این پنل، سازمان‌ها و کسب‌وکارها می‌توانند برای پیک و خودرو تعداد نامحدود درخواست ثبت کنند.
+به‌سادگی می‌توانید از آن برای ارسال کالاهای خود درون‌شهری و انجام ماموریت‌ها استفاده کرده و آن‌ها را مدیریت کنید.
+ </p>
+</article>
+  </div>
+
+<div class="tab-content" id="nav-delivery-tab">
+  <article class="feature-images-box">
+  <img class="feature-images" src="/images/cc-delivery.webp" alt="featureimage">
+</article>
+<article class="feature-content">
+ <h3 class="services">خدمات تپسی برای کسب و کارها</h3>
+ <p class="feature-text">پنل سازمانی تپسی، سامانه‌ای تحت وب است که امکان درخواست هم‌زمان و سهولت استفاده را فراهم می‌کند.
+با این پنل، سازمان‌ها و کسب‌وکارها می‌توانند برای پیک و خودرو تعداد نامحدود درخواست ثبت کنند.
+به‌سادگی می‌توانید از آن برای ارسال کالاهای خود درون‌شهری و انجام ماموریت‌ها استفاده کرده و آن‌ها را مدیریت کنید.
+ </p>
+</article>
+  </div>
+
+  <div class="tab-content" id="nav-motopeyktab-tab">
+  <article class="feature-images-box">
+  <img class="feature-images" src="/images/cc-intercity.webp" alt="featureimage">
+</article>
+<article class="feature-content">
+ <h3 class="services">خدمات تپسی برای کسب و کارها</h3>
+ <p class="feature-text">پنل سازمانی تپسی، سامانه‌ای تحت وب است که امکان درخواست هم‌زمان و سهولت استفاده را فراهم می‌کند.
+با این پنل، سازمان‌ها و کسب‌وکارها می‌توانند برای پیک و خودرو تعداد نامحدود درخواست ثبت کنند.
+به‌سادگی می‌توانید از آن برای ارسال کالاهای خود درون‌شهری و انجام ماموریت‌ها استفاده کرده و آن‌ها را مدیریت کنید.
+ </p>
+</article>
+  </div>
+
+  <div class="tab-content" id="nav-plus-tab">
+  <article class="feature-images-box">
+  <img class="feature-images" src="/images/cc-plus.webp" alt="featureimage">
+</article>
+<article class="feature-content">
+ <h3 class="services">خدمات تپسی برای کسب و کارها</h3>
+ <p class="feature-text">پنل سازمانی تپسی، سامانه‌ای تحت وب است که امکان درخواست هم‌زمان و سهولت استفاده را فراهم می‌کند.
+با این پنل، سازمان‌ها و کسب‌وکارها می‌توانند برای پیک و خودرو تعداد نامحدود درخواست ثبت کنند.
+به‌سادگی می‌توانید از آن برای ارسال کالاهای خود درون‌شهری و انجام ماموریت‌ها استفاده کرده و آن‌ها را مدیریت کنید.
+ </p>
+</article>
+  </div>
+
+  <div class="tab-content" id="nav-tell-tab">
+  <article class="feature-images-box">
+  <img class="feature-images" src="/images/cc-tel.webp" alt="featureimage">
+</article>
+<article class="feature-content">
+ <h3 class="services">خدمات تپسی برای کسب و کارها</h3>
+ <p class="feature-text">پنل سازمانی تپسی، سامانه‌ای تحت وب است که امکان درخواست هم‌زمان و سهولت استفاده را فراهم می‌کند.
+با این پنل، سازمان‌ها و کسب‌وکارها می‌توانند برای پیک و خودرو تعداد نامحدود درخواست ثبت کنند.
+به‌سادگی می‌توانید از آن برای ارسال کالاهای خود درون‌شهری و انجام ماموریت‌ها استفاده کرده و آن‌ها را مدیریت کنید.
+ </p>
+</article>
+  </div>
+
+  <div class="tab-content" id="nav-wait-tab">
+  <article class="feature-images-box">
+  <img class="feature-images" src="/images/cc-wait-and-share.webp" alt="featureimage">
+</article>
+<article class="feature-content">
+ <h3 class="services">خدمات تپسی برای کسب و کارها</h3>
+ <p class="feature-text">پنل سازمانی تپسی، سامانه‌ای تحت وب است که امکان درخواست هم‌زمان و سهولت استفاده را فراهم می‌کند.
+با این پنل، سازمان‌ها و کسب‌وکارها می‌توانند برای پیک و خودرو تعداد نامحدود درخواست ثبت کنند.
+به‌سادگی می‌توانید از آن برای ارسال کالاهای خود درون‌شهری و انجام ماموریت‌ها استفاده کرده و آن‌ها را مدیریت کنید.
+ </p>
+</article>
+  </div>
+
+</div>
