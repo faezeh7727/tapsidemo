c3830134e0c86a2af6e9f2e46b00615777d32faa
faezeh7727 <gfaeze561@gmail.com>
2025-10-30 12:52:37 -0700

    accordion section

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
-const toggler=document.querySelector(".nav-toggler");
-const nav=document.querySelector(".nav");
-toggler.addEventListener("click",(e)=>{
-    nav.classList.toggle('nav-expanded');
-})
+const accordions = document.querySelectorAll(".accordion");
+
+accordions.forEach((item) => {
+  const header = item.querySelector(".accordion-header");
+
+  header.addEventListener("click", () => {
+    // بستن همه آکاردیون‌ها
+    accordions.forEach((acc) => {
+      if (acc !== item) acc.classList.remove("accordion-expanded");
+    });
+
+    // باز یا بسته کردن فقط آکاردیون فعلی
+    item.classList.toggle("accordion-expanded");
+  });
+});
